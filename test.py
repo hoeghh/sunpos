9de7395494abd209150d280efd23933a8657b37c
Henrik René Høegh <heh@praqma.net>
2018-08-17 21:10:53 +0200

    Added evelation

--- test.py
+++ test.py
@@ -2,9 +2,10 @@
 from pysolar.solar import *
 import datetime
 
-# your location (Example Romalt in Denmark)
+# your location (Example Romalt in Denmark, 0 meters above sealevel)
 lat = 56.4444472
 lon = 10.0838776
+eve = 0
 
 # Time section
 # Use now or specifi time
@@ -23,6 +24,11 @@ print("Datetime : ", date)
 # As Pysolar uses south as Zero with west as negative and east as positive
 # we multiply with -1 and adds 180 to recalibrate to North as our point of zero
 # http://docs.pysolar.org/en/latest/#examples
-print("Altitude : " , get_altitude(lat, lon, date))
+# get_altitude is given 3 parameters :
+#   lat - Latitude
+#   lon - Longitude
+#   eve - Evelation
+
+print("Altitude : " , get_altitude(lat, lon, date, eve))
 print("Azimuth  : ", -1 * get_azimuth(lat, lon, date) + 180)
 
